8dad0daf72e0d41db6e11aed09049529d9cf0e8c
Fernando Santos <f.bsantos2505@gmail.com>
2024-10-19 16:35:55 -0300

    Conclusao M29

--- src/components/FormVagas/index.tsx
+++ src/components/FormVagas/index.tsx
@@ -1,6 +1,6 @@
 import { FormEvent, useState } from 'react'
 
-import styles from './FormVagas.module.css'
+import { FormularioPesquisa, CampoPesquisar, BotaoPesquisar } from './styles'
 
 type Props = {
   aoPesquisar: (termo: string) => void
@@ -15,17 +15,14 @@ const FormVagas = ({ aoPesquisar }: Props) => {
   }
 
   return (
-    <form className={styles.form} onSubmit={aoEnviarForm}>
-      <input
-        className={styles.campo}
+    <FormularioPesquisa onSubmit={aoEnviarForm}>
+      <CampoPesquisar
         placeholder="Front-end, fullstack, node, design"
         onChange={(e) => setTermo(e.target.value)}
         type="search"
       />
-      <button className={styles.btnPesquisar} type="submit">
-        Pesquisar
-      </button>
-    </form>
+      <BotaoPesquisar type="submit">Pesquisar</BotaoPesquisar>
+    </FormularioPesquisa>
   )
 }
 export default FormVagas
---
+++ src/containers/theme/origin.ts
@@ -0,0 +1,7 @@
+const themeOrigin = {
+  corPrincipal: '#a7727d',
+  corSecundaria: '#f9f5e7',
+  corTextoHero: '#eee'
+}
+
+export default themeOrigin
